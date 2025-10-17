dc5b678d863ef6f7e34818b107bc8320ba4fcc33
RAQUEL DA SILVA <raquel_silva10@estudante.sesisenai.org.br>
2025-10-17 19:45:40 -0300

    criando a entidade do Produto

---
+++ src/main/java/com/weg/gestao_loja/model/Produto.java
@@ -0,0 +1,40 @@
+package com.weg.gestao_loja.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    @Lob
+    private String descricao;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private double preco;
+
+    @Column(nullable = false)
+    private int quantidade;
+
+    public Produto(String nome, String descricao, double preco, int quantidade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+}
