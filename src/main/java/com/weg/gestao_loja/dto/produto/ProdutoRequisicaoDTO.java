5c2e44f681ea3e9edc469c17155c35898fbddb81
RAQUEL DA SILVA <raquel_silva10@estudante.sesisenai.org.br>
2025-10-17 19:51:28 -0300

    criando o dto de requisicao do Produto

---
+++ src/main/java/com/weg/gestao_loja/dto/produto/ProdutoRequisicaoDTO.java
@@ -0,0 +1,24 @@
+package com.weg.gestao_loja.dto.produto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record ProdutoRequisicaoDTO(
+        @NotBlank(message = "O nome é obrigatório!")
+        String nome,
+
+        @NotBlank(message = "A descrição é obrigatória!")
+        String descricao,
+
+        @NotNull(message = "O preço é obrigatório!")
+        @Positive
+        double preco,
+
+        @NotNull(message = "A quantidade é obrigatória!")
+        @PositiveOrZero
+        int quantidade
+) {
+
+}
