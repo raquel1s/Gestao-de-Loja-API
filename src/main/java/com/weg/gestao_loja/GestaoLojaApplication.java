2513c5ddbc365bac0aa0f523eb83f8da4341b9ea
RAQUEL DA SILVA <raquel_silva10@estudante.sesisenai.org.br>
2025-10-17 19:36:55 -0300

    first commit

---
+++ src/main/java/com/weg/gestao_loja/GestaoLojaApplication.java
@@ -0,0 +1,13 @@
+package com.weg.gestao_loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoLojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weg/gestao_loja/GestaoLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.gestao_loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
