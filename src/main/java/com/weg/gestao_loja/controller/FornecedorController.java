989c0d6f086d8acee1ddc1e830fb5d7e0f741738
RAQUEL DA SILVA <raquel_silva10@estudante.sesisenai.org.br>
2025-10-21 16:43:25 -0300

    Criando o o controller do fornecedor

--- src/main/java/com/weg/gestao_loja/controller/FornecedorController.java
+++ src/main/java/com/weg/gestao_loja/controller/FornecedorController.java
@@ -1,11 +1,46 @@
 package com.weg.gestao_loja.controller;
 
+import com.weg.gestao_loja.dto.fornecedor.FornecedorRequisicaoDTO;
+import com.weg.gestao_loja.dto.fornecedor.FornecedorRespostaDTO;
+import com.weg.gestao_loja.service.FornecedorService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
 @AllArgsConstructor
 public class FornecedorController {
+
+    private final FornecedorService service;
+
+    @PostMapping
+    public ResponseEntity<FornecedorRespostaDTO> criar(@RequestBody FornecedorRequisicaoDTO requisicaoDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.criar(requisicaoDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FornecedorRespostaDTO>> buscarTodos(){
+        return ResponseEntity.status(HttpStatus.OK).body(service.buscarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FornecedorRespostaDTO> buscarPorId(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(service.buscarPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FornecedorRespostaDTO> atualizar(@PathVariable Long id,
+                                                        @RequestBody FornecedorRequisicaoDTO requisicaoDTO){
+        return ResponseEntity.status(HttpStatus.OK).body(service.atualizar(id, requisicaoDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id){
+        service.excluir(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
