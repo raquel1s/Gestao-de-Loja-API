d185bff325682aa6414eca80243dee693a334703
RAQUEL DA SILVA <raquel_silva10@estudante.sesisenai.org.br>
2025-10-21 16:56:27 -0300

    adicionando rota

--- src/main/java/com/weg/gestao_loja/controller/FornecedorController.java
+++ src/main/java/com/weg/gestao_loja/controller/FornecedorController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/fornecedores")
 @AllArgsConstructor
 public class FornecedorController {
 
