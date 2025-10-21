46d160ce916747dc678e4af8d115f6cc5998f777
RAQUEL DA SILVA <raquel_silva10@estudante.sesisenai.org.br>
2025-10-21 14:57:32 -0300

    criando o repository de categoria

---
+++ src/main/java/com/weg/gestao_loja/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.weg.gestao_loja.repository;
+
+import com.weg.gestao_loja.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
