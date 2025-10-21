10430ac3860a54fd997e8a08e7058d6390b5ba9f
RAQUEL DA SILVA <raquel_silva10@estudante.sesisenai.org.br>
2025-10-21 14:53:59 -0300

    Criando o mapper de categoria

--- src/main/java/com/weg/gestao_loja/mapper/CategoriaMapper.java
+++ src/main/java/com/weg/gestao_loja/mapper/CategoriaMapper.java
@@ -1,4 +1,30 @@
 package com.weg.gestao_loja.mapper;
 
+import com.weg.gestao_loja.dto.categoria.CategoriaRequisicaoDTO;
+import com.weg.gestao_loja.dto.categoria.CategoriaRespostaDTO;
+import com.weg.gestao_loja.model.Categoria;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CategoriaMapper {
+
+    public Categoria paraEntidade(CategoriaRequisicaoDTO requisicaoDTO){
+        return new Categoria(requisicaoDTO.nome(), requisicaoDTO.descricao());
+    }
+
+    public CategoriaRespostaDTO paraRespostaDTO(Categoria categoria){
+        return new CategoriaRespostaDTO(categoria.getId(), categoria.getNome(), categoria.getDescricao());
+    }
+
+    public Categoria paraUpdate(Categoria categoria, CategoriaRequisicaoDTO requisicaoDTO){
+        if(!requisicaoDTO.nome().equals(categoria.getNome())){
+            categoria.setNome(requisicaoDTO.nome());
+        }
+
+        if(!requisicaoDTO.descricao().equals(categoria.getDescricao())){
+            categoria.setDescricao(requisicaoDTO.descricao());
+        }
+
+        return categoria;
+    }
 }
